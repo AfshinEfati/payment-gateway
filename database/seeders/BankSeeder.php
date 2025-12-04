1b235aaa56d6d3ac159358c1075b91df641b3d02
afshin <afshin.efati@gmail.com>
2025-12-04 06:03:57 +0330

    auto: 2025-12-04 06:03
     M database/seeders/BankSeeder.php
     M database/seeders/GatewaySeeder.php

--- database/seeders/BankSeeder.php
+++ database/seeders/BankSeeder.php
@@ -14,9 +14,22 @@ class BankSeeder extends Seeder
             ['name_en' => 'Saman', 'name_fa' => 'سامان', 'code' => 'saman'],
             ['name_en' => 'Pasargad', 'name_fa' => 'پاسارگاد', 'code' => 'pasargad'],
             ['name_en' => 'Parsian', 'name_fa' => 'پارسیان', 'code' => 'parsian'],
+            ['name_en' => 'Melli', 'name_fa' => 'ملی', 'code' => 'melli'],
+            ['name_en' => 'Tejarat', 'name_fa' => 'تجارت', 'code' => 'tejarat'],
+            ['name_en' => 'Saderat', 'name_fa' => 'صادرات', 'code' => 'saderat'],
+            ['name_en' => 'Sepah', 'name_fa' => 'سپه', 'code' => 'sepah'],
+            ['name_en' => 'Keshavarzi', 'name_fa' => 'کشاورزی', 'code' => 'keshavarzi'],
+            ['name_en' => 'Maskan', 'name_fa' => 'مسکن', 'code' => 'maskan'],
             ['name_en' => 'Ayandeh', 'name_fa' => 'آینده', 'code' => 'ayandeh'],
             ['name_en' => 'Eghtesad Novin', 'name_fa' => 'اقتصاد نوین', 'code' => 'enbank'],
-            ['name_en' => 'Melli', 'name_fa' => 'ملی', 'code' => 'melli'],
+            ['name_en' => 'Karafarin', 'name_fa' => 'کارآفرین', 'code' => 'karafarin'],
+            ['name_en' => 'Sina', 'name_fa' => 'سینا', 'code' => 'sina'],
+            ['name_en' => 'Shahr', 'name_fa' => 'شهر', 'code' => 'shahr'],
+            ['name_en' => 'Dey', 'name_fa' => 'دی', 'code' => 'day'],
+            ['name_en' => 'Sarmayeh', 'name_fa' => 'سرمایه', 'code' => 'sarmayeh'],
+            ['name_en' => 'Iran Zamin', 'name_fa' => 'ایران زمین', 'code' => 'iranzamin'],
+            ['name_en' => 'Resalat', 'name_fa' => 'رسالت', 'code' => 'resalat'],
+            ['name_en' => 'Refah', 'name_fa' => 'رفاه', 'code' => 'refah'],
         ];
 
         DB::table(config('payment.tables.banks', 'banks'))->insertOrIgnore($banks);
--- database/seeders/GatewaySeeder.php
+++ database/seeders/GatewaySeeder.php
@@ -9,13 +9,13 @@ class GatewaySeeder extends Seeder
 {
     public function run()
     {
-        // Ensure banks exist first
-        $mellatId = DB::table(config('payment.tables.banks', 'banks'))->where('code', 'mellat')->value('id');
-        $samanId = DB::table(config('payment.tables.banks', 'banks'))->where('code', 'saman')->value('id');
+        // Fetch Bank IDs
+        $banks = DB::table(config('payment.tables.banks', 'banks'))->pluck('id', 'code');
 
         $gateways = [
+            // PSPs (Direct Bank Gateways)
             [
-                'bank_id' => $mellatId,
+                'bank_id' => $banks['mellat'] ?? null,
                 'name_en' => 'Mellat IPG',
                 'name_fa' => 'درگاه پرداخت ملت',
                 'driver' => 'mellat',
@@ -23,7 +23,7 @@ class GatewaySeeder extends Seeder
                 'config' => json_encode([]),
             ],
             [
-                'bank_id' => $samanId,
+                'bank_id' => $banks['saman'] ?? null,
                 'name_en' => 'Saman SEP',
                 'name_fa' => 'درگاه پرداخت سامان',
                 'driver' => 'saman',
@@ -31,10 +31,84 @@ class GatewaySeeder extends Seeder
                 'config' => json_encode([]),
             ],
             [
-                'bank_id' => null, // Zarinpal is not a bank
+                'bank_id' => $banks['melli'] ?? null,
+                'name_en' => 'Sadad (Melli)',
+                'name_fa' => 'درگاه پرداخت سداد (ملی)',
+                'driver' => 'sadad',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => $banks['pasargad'] ?? null,
+                'name_en' => 'Pasargad PEP',
+                'name_fa' => 'درگاه پرداخت پاسارگاد',
+                'driver' => 'pasargad',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => $banks['parsian'] ?? null,
+                'name_en' => 'Parsian PEC',
+                'name_fa' => 'درگاه پرداخت پارسیان',
+                'driver' => 'parsian',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => null, // IranKish connects to multiple banks
+                'name_en' => 'IranKish',
+                'name_fa' => 'ایران کیش',
+                'driver' => 'irankish',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => null, // AsanPardakht connects to multiple banks
+                'name_en' => 'AsanPardakht',
+                'name_fa' => 'آسان پرداخت',
+                'driver' => 'asanpardakht',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+
+            // Intermediaries (Payment Facilitators)
+            [
+                'bank_id' => null,
                 'name_en' => 'Zarinpal',
                 'name_fa' => 'زرین‌پال',
                 'driver' => 'zarinpal',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => null,
+                'name_en' => 'IDPay',
+                'name_fa' => 'آیدی پی',
+                'driver' => 'idpay',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => null,
+                'name_en' => 'NextPay',
+                'name_fa' => 'نکست پی',
+                'driver' => 'nextpay',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => null,
+                'name_en' => 'PayIr',
+                'name_fa' => 'پی دات آی آر',
+                'driver' => 'payir',
+                'is_active' => true,
+                'config' => json_encode([]),
+            ],
+            [
+                'bank_id' => null,
+                'name_en' => 'PayPing',
+                'name_fa' => 'پی پینگ',
+                'driver' => 'payping',
                 'is_active' => true,
                 'config' => json_encode([]),
             ],
